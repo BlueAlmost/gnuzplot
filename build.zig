aeb4d75e2905f3402a0c34eed9f778ca0985c33c
BlueAlmost <git.blue.almost@gmail.com>
2023-02-10 22:08:02 +0100

    aligned with new build process

--- build.zig
+++ build.zig
@@ -1,75 +1,45 @@
 const std = @import("std");
-const Builder = std.build.Builder;
-const Pkg = std.build.Pkg;
-
-const pkgs = struct {
-    const gnuzplot = Pkg{
-        .name = "gnuzplot",
-        .source = .{ .path = "./gnuzplot.zig" },
-        .dependencies = null,
-        // .dependencies = &[_]Pkg {
-        //     Pkg {
-        //         .name = "LinAlg",
-        //         .source = .{ .path = "../zLinAlg/zLinAlg.zig" },
-        //         .dependencies = null,
-        //     },
-        // },
-    };
-};
 
 pub fn build(b: *std.build.Builder) void {
+
     const target = b.standardTargetOptions(.{});
-    const mode = b.standardReleaseOptions();
 
-    const exe_targets = [_]Target{
-        .{ .name = "examples", .src = "example/examples.zig", .desc = "gnuzplot example" },
-    };
+    // available options: Debug, ReleaseSafe, ReleaseFast, ReleaseSmall
 
-    for (exe_targets) |e_target| {
-        e_target.build(b);
-    }
+    const optimize = b.standardOptimizeOption(.{});
+    // const optimize = std.builtin.Mode.Debug;
+    // const optimize = std.builtin.Mode.ReleaseSafe;
+    // const optimize = std.builtin.Mode.ReleaseFast;
+    // const optimize = std.builtin.Mode.ReleaseSmall;
 
-    const exe_tests = b.addTest("main_test.zig");
+    // example executable --------------------------------------------
 
-    exe_tests.setTarget(target);
-    exe_tests.setBuildMode(mode);
+    const exe_example = b.addExecutable(.{
+        .name = "examples",
+        .root_source_file = .{.path = "./example/examples.zig"},
+        .target = target,
+        .optimize = optimize,
+    });
 
-    exe_tests.addPackage(pkgs.gnuzplot);
+    exe_example.addAnonymousModule("gnuzplot", .{
+        .source_file = .{.path = "./gnuzplot.zig"},
+    });
 
-    const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&exe_tests.step);
+    b.exe_dir = b.pathFromRoot("./example");
+    exe_example.install();
+
+    // unit tests ----------------------------------------------------
+    const unit_tests = b.addTest(.{
+        .root_source_file = .{.path = "./main_test.zig"},
+        .target = target,
+        .optimize = optimize,
+    });
+
+    unit_tests.addAnonymousModule("gnuzplot", .{
+        .source_file = .{.path = "./gnuzplot.zig"},
+    });
+
+    const unit_tests_step = b.step("test", "Run unit tests");
+    unit_tests_step.dependOn(&unit_tests.step);
+
 }
-
-//---------------------------------------------------------------------------
-
-const Target = struct {
-    name: []const u8,
-    src: []const u8,
-    desc: []const u8,
-
-    pub fn build(self: Target, b: *std.build.Builder) void {
-
-        // write executables here instead of "zig-out/bin"
-        b.exe_dir = b.pathFromRoot("./example");
-
-        // const target = b.standardTargetOptions(.{});
-        const mode = b.standardReleaseOptions();
-
-        var exe = b.addExecutable(self.name, self.src);
-
-        exe.setBuildMode(b.standardReleaseOptions());
-        // exe.setTarget(target);
-        exe.setBuildMode(mode);
-
-        // gnuzplot
-        exe.addPackage(pkgs.gnuzplot);
-
-        exe.install();
-
-        const run_cmd = exe.run();
-        run_cmd.step.dependOn(b.getInstallStep());
-        if (b.args) |args| {
-            run_cmd.addArgs(args);
-        }
-    }
-};
